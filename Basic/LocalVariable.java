511aa6ba6c0629253f6620f5a60c85518faa4403
Praveen singh <praveensingh005@gmail.com>
2021-04-26 23:05:17 +0530

    git commit -m "hey"

---
+++ Basic/LocalVariable.java
@@ -0,0 +1,32 @@
+public class LocalVariable {
+    
+    public void Student()
+    {
+        int age = 20;
+        age +=5;
+        System.out.println("The student age is : "+age);
+    }
+    public static void main(String[] args)
+    {
+      LocalVariable obj = new LocalVariable();
+      obj. Student();
+    }
+   
+    
+}
+
+
+/*public class LocalVariable  {
+    public void StudentAge()
+    {
+        // local variable age
+        int age = 0;
+        age = age + 5;
+        System.out.println("Student age is : " + age);
+    }
+  
+    public static void main(String args[])
+    {
+        LocalVariable obj = new LocalVariable();
+        obj.StudentAge();
+    }}*/
