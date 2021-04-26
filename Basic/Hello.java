6fcb5c617c600e09c298c65fe328cc178522a7af
Praveen singh <praveensingh005@gmail.com>
2021-04-26 20:06:41 +0530

    git commit -m "first"

---
+++ Basic/Hello.java
@@ -0,0 +1,17 @@
+
+  public class Hello {
+    public static void main(String[] args)
+    {
+        
+        boolean b = true;
+        if(b == true)
+        {
+            System.out.println("hello Geeks For Geeks");
+        }
+        else
+        {
+            System.out.println("Fuck You");
+        }
+    }
+    
+}
