f8ac1a441e52cfd948aeaf41e33202a3a6e89174
Prashant Tiwari <prashanttiwari4398@gmail.com>
2021-04-27 21:36:40 +0530

    update

--- Basic/StaticVariable.java
+++ Basic/StaticVariable.java
@@ -1,4 +1,4 @@
-
+/*
 class Mark{
     public static int Number;
     public static String Name = "Prashant Tiwari";
@@ -15,3 +15,29 @@ public class StaticVariable {
     }
     
 }
+ */
+/*class Brand{
+    public static int brand_no;
+    public static String Name = "lamborghini";
+}
+public class StaticVariable{
+     public static void main(String[] args) {
+    
+        Brand.brand_no = 8769;
+        System.out.println(Brand.Name+" plate number is "+Brand.brand_no);
+    }
+}
+*/
+class Mathematics{
+
+    public static int Mark;
+    public static String name1 = "Math";
+     
+}
+   public class StaticVariable{
+     public static void main(String[] args)
+     {
+       Mathematics.Mark = 98;
+       
+       System.out.println(Mathematics.name1+" get marks : "+Mathematics.Mark);
+     }}
